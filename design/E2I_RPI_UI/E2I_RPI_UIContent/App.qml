dca5509ff6be04c9ecd782132f035f73e74fe072
Park <parkchuy7077@gmail.com>
2024-09-22 21:05:55 +0900

    ¶ó¼Â ¹öư°ú °°­Á¦ ³«Çϻê »çÃâ ¹öưUI Ã߰¡

---
+++ design/E2I_RPI_UI/E2I_RPI_UIContent/App.qml
@@ -0,0 +1,19 @@
+// Copyright (C) 2021 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+import QtQuick 6.5
+import E2I_RPI_UI
+
+Window {
+    width: mainScreen.width
+    height: mainScreen.height
+
+    visible: true
+    title: "E2I_RPI_UI"
+
+    Screen01 {
+        id: mainScreen
+    }
+
+}
+
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,21 +1,45 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainUI_designzwNdeU.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
 
 
-class MyApp(QWidget):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(1366, 768)
+        MainWindow.setStyleSheet(u"bakground-color: rgb(31, 36, 50)")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1366, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+        self.retranslateUi(MainWindow)
 
-    def initUI(self):
-        self.setWindowTitle('Avionics_RPI')
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
 
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+    # retranslateUi
 
-if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
