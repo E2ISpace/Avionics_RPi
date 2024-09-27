2924b295e7d37fda2941372260757a4ff4f960c2
park-chu <parkchuy7077@kyonggi.ac.kr>
2024-09-27 13:33:20 +0900

    Update MainUI.py

--- GUI_script/MainUI.py
+++ GUI_script/MainUI.py
@@ -1,31 +1,27 @@
 import sys
+import serial
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 
-import serial
-
 # uic.loadUiType은 두 개의 값을 반환하므로 튜플을 풀어줍니다.
-form_class, base_class = uic.loadUiType("D:\\EEI\\Avionics_RPi\\RPI_GUI\\MainUI.ui")
+form_class, base_class = uic.loadUiType("/home/park/E2I_RPI_WorkSpace/Avionics_RPi/RPI_GUI/MainUI.ui")
 
 class WindowClass(QMainWindow, form_class):
-
-   # self.serial_port = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # 적절한 포트로 변경
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.serial_port = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # 적절한 포트로 변경
     
     def ResetStart(self):
         self.textEdit.append("RESET")
-
         ResetCode = "RESET"
-"""
+
         if self.serial_port.is_open:
             self.serial_port.write(ResetCode.encode())
             print(f"Sent message: {ResetCode}")
         else:
             print("Serial port is not open")
-"""
 
 # QApplication 인스턴스 생성 시 sys.argv를 사용합니다.
 app = QApplication(sys.argv)
@@ -36,5 +32,3 @@ main_window.show()
 
 # 이벤트 루프 실행
 app.exec_()
-
-
