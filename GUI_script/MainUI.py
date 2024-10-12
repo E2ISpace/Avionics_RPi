e4d23f97ec4e6d46e7a6eafd3fd2a1f907b696dc
park-chu <parkchuy7077"kyonggi.ac.kr>
2024-10-12 12:28:33 +0100

    Reset event

--- GUI_script/MainUI.py
+++ GUI_script/MainUI.py
@@ -4,14 +4,16 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 
 # uic.loadUiType은 두 개의 값을 반환하므로 튜플을 풀어줍니다.
-form_class, base_class = uic.loadUiType("/home/park/E2I_RPI_WorkSpace/Avionics_RPi/RPI_GUI/MainUI.ui")
+
+
+form_class, base_class = uic.loadUiType("/home/E2I/E2I_WorkSpace/Avionics_RPI/RPI_GUI/MainUI.ui")
 
 class WindowClass(QMainWindow, form_class):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.serial_port = serial.Serial('/dev/ttyACM0', 9600, timeout=1)  # 적절한 포트로 변경
+        
     
     def ResetStart(self):
         self.textEdit.append("RESET")
